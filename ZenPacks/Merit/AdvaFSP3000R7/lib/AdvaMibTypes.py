0f67262042a2156092da655b11bcee111ad57332
Merit Network, Inc <software-dev@merit.edu>
2020-08-17 11:28:25 -0400

    Revert "Added codes for adminState"
    This reverts commit 93012591d7bfdeb32e1cad929ab1b63ddd907a78.

--- ZenPacks/Merit/AdvaFSP3000R7/lib/AdvaMibTypes.py
+++ ZenPacks/Merit/AdvaFSP3000R7/lib/AdvaMibTypes.py
@@ -1,21 +1,6 @@
 """
-Code translations for defined types in ADVA MIBs
+Code translations for defined types in ADVA-MIB
 """
-
-class AdminState:
-    """
-    http://www.circitor.fr/Mibs/Html/A/ADVA-FSPR7-TC-MIB.php#FspR7AdminState
-    Translations assisted by browsing the Adva FSP Network Manager client.
-    """
-    UNDEFINED = 0
-    UNASSIGNED = 1
-    IN_SERVICE = 2
-    AUTO_IN_SERVICE = 3
-    OUT_OF_SERVICE_MANAGEMENT = 4
-    OUT_OF_SERVICE_MAINTENANCE = 5
-    DISABLED = 6
-    PRE_POST_SERVICE = 7
-
 
 class AssignmentState:
     """
